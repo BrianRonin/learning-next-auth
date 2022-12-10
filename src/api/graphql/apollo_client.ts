ff43375c8d19ec27f877b40e464b4197cdd05928
BrianRonin <01100010v@gmail.com>
2022-12-10 05:35:47 -0300

    up

--- src/api/graphql/apollo_client.ts
+++ src/api/graphql/apollo_client.ts
@@ -1,11 +1,17 @@
-import { onError } from '@apollo/client/link/error'
 import {
+  ApolloCache,
   ApolloClient,
   ApolloLink,
   createHttpLink,
+  DefaultContext,
   from,
   InMemoryCache,
+  MutationOptions,
+  OperationVariables,
+  QueryOptions,
 } from '@apollo/client'
+import { auth } from './Auth/mutations'
+import { Auth } from '../../types/nextAuth'
 
 const system = createHttpLink({
   uri: 'http://localhost:8055/graphql/system',
@@ -17,13 +23,14 @@ const base = createHttpLink({
 
 const authLink = new ApolloLink(
   (operation, forward) => {
-    if (!operation.getContext().auth)
-      return forward(operation)
+    const _token = operation.getContext().auth
+    const token = _token
+      ? _token
+      : '3m3Z3tVlk-9ZTwyRHoYj4WCLHQctWYZV'
     operation.setContext(({ headers }: any) => ({
       headers: {
         ...headers,
-        Authorization:
-          'Bearer ' + operation.getContext().auth, // however you get your token
+        Authorization: 'Bearer ' + token, // however you get your token
       },
     }))
     return forward(operation)
@@ -34,7 +41,49 @@ const authLink = new ApolloLink(
   base,
 )
 
-export const Client = new ApolloClient({
+export const getToken = async (credentials: {
+  email: string
+  password: string
+}): Promise<string> => {
+  const { data } = await _Client.mutate({
+    mutation: auth,
+    variables: credentials,
+    context: { system: true },
+  })
+  return data.auth_login.access_token
+}
+
+export const _Client = new ApolloClient({
   link: from([authLink]),
   cache: new InMemoryCache(),
 })
+_Client.query
+
+export const Client = async (
+  action?: {
+    mutate?: MutationOptions<
+      any,
+      OperationVariables,
+      DefaultContext,
+      ApolloCache<any>
+    >
+    query?: QueryOptions<any>
+  },
+  auth?: Auth,
+): Promise<any> => {
+  let token = null
+  const method = action?.mutate
+    ? 'mutate'
+    : 'query'
+  if (auth) token = await getToken(auth)
+  // @ts-ignore
+  return _Client[method]({
+    // @ts-ignore
+    ...action[method],
+    context: {
+      auth: token,
+      // @ts-ignore
+      ...action[method]?.context,
+    },
+  })
+}
